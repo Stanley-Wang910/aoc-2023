e7dee5207b12eeaabc91c5dda066f7a8c672dfae
twinfantasyfan <wangstanley910@gmail.com>
2024-09-20 19:25:55 -0400

    day one completed using regex

--- aoc-2023-java/src/main/java/Day1/Trebuchet.java
+++ aoc-2023-java/src/main/java/Day1/Trebuchet.java
@@ -5,17 +5,36 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Trebuchet {
+    private static final Map<String, String> numSpellMap = new HashMap<String, String>() {{
+        put("zero", "0");
+        put("one", "1");
+        put("two", "2");
+        put("three", "3");
+        put("four", "4");
+        put("five", "5");
+        put("six", "6");
+        put("seven", "7");
+        put("eight", "8");
+        put("nine", "9");
+    }};
 
     public static void main(String[] args) {
         ArrayList<String> input  = readInput();
-        int sumDigits = sumFirstLastDigits(input);
+//        int sumDigits = part1(input);
+        int sumDigits = part2(input);
         System.out.println(sumDigits);
     }
 
-    private static int sumFirstLastDigits(ArrayList<String> input) {
+    private static int part1(ArrayList<String> input) {
         int sum = 0;
         for (String s : input) {
             String firstInt = "";
@@ -45,10 +64,36 @@ public class Trebuchet {
         return sum;
     }
 
+    private static int part2(ArrayList<String> input) {
+        int sum = 0;
+        Pattern pattern = Pattern.compile("(?=(\\d|one|two|three|four|five|six|seven|eight|nine))");
+
+        for (String s : input) {
+            ArrayList<String> matches = new ArrayList<>();
+            Matcher matcher = pattern.matcher(s);
+
+            while (matcher.find()) {
+                String match = matcher.group(1);
+                matches.add(numSpellMap.getOrDefault(match,match));
+            }
+
+            String firstInt = matches.get(0);
+            String lastInt = matches.get(matches.size()-1);
+
+            String combined = firstInt + lastInt;
+
+            sum += Integer.parseInt(combined);
+
+        }
+    return sum;
+    }
+
+
+
     public static ArrayList<String> readInput() {
         FileInputStream stream = null;
         try {
-            stream = new FileInputStream("/Users/stanley/Code/aoc-2023-java/src/main/java/Day1/Day1Input");
+            stream = new FileInputStream("/Users/stanley/Code/aoc-2023/aoc-2023-java/src/main/java/Day1/Day1Input");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
